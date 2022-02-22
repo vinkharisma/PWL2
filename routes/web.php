f5a4b6d9d3cd7751ff35ad19bdc9624dc558e682
vinkharisma <vinkharismadzania@gmail.com>
2022-02-23 00:34:18 +0700

    Commit 1

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Default route bawaan create project laravel
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route view
+Route::view('/home', 'viewName');
+
+// Route redirect
+Route::redirect('/lama', '/baru', 301);
+
+// Route parameter
+Route::get('/user/{id}', function ($id) {
+    return 'User ID: ' . $id;
+});
+
+// Route with optional parameter
+Route::get('/user/{id?}', function ($id = null) {
+    return 'User ID: ' . $id;
+});
+
+// Route with required parameter and controller laravel 8
+Route::get('/user/{id}', [UserController::class, 'show']);
+ 
